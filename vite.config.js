8c3fc5d7613109d88cb41a93fd679efcea1a6b99
Shruti Joshi <shrutijoshi1481995@gmail.com>
2026-06-28 20:56:50 +0530

    Splited libraries

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three')) return 'vendor-three';
+            if (id.includes('gsap')) return 'vendor-gsap';
+            return 'vendor-core';
+          }
+        }
+      }
+    }
+  }
 })
